1eb4f41fb78699ecbf32ff92ab7e6aa23211ec19
nikhil5467 <nikhilrocks374@gmail.com>
2017-05-23 23:51:13 +0530

    session12assignment4
    session12assignment4

---
+++ case-pipe.component.ts
@@ -0,0 +1,13 @@
+import {Component} from 'angular2/core';
+@Component({
+  selector: 'case-pipe',
+  template: `
+	    <h2>Lower and Upper case Pipe Example</h2>
+	    <p>In lowerCase : {{str | lowercase}}</p>
+	    <p>In uppercase : {{str | uppercase}}</p>
+	    `
+})
+
+export class LowerUpperCasePipe {
+	str: string = "My name is romil, I love JavaScript";
+}
